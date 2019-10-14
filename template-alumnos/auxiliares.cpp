5bc552d15837febffae871f993c56a3b9b3a6368
Patricio Bruno <pdbruno@gmail.com>
2019-10-14 15:55:56 -0300

    puntos 4 y 5 terminados

--- template-alumnos/auxiliares.cpp
+++ template-alumnos/auxiliares.cpp
@@ -25,14 +25,14 @@ bool esBinaria(const imagen& A){
 
 bool sonPixelesConectadosViniendoDe(const imagen& A, const pixel& p, const pixel& q, int k, vector<pixel> blacklist){
     bool res = false;
-    if(k==8){
+    if(k == 8){
         if(max(abs(p[0] - q[0]), abs(p[1] - q[1])) <= 1){
             res = true;
         } else {
             blacklist.push_back(p);
             for (int i = p[0]-1; i < p[0]+2; ++i) {
                 for (int j = p[1]-1; j < p[1]+2; ++j) {
-                    if(pixelValidoEncendido(A, {i, j}) && notIn(blacklist, {i, j})){
+                    if(pixelValidoEncendido(A, {i, j}) && !pertenece(blacklist, {i, j})){
                         blacklist.push_back({i, j});
                         res |= sonPixelesConectadosViniendoDe(A, {i, j}, q, 8, blacklist);
                     }
@@ -45,20 +45,20 @@ bool sonPixelesConectadosViniendoDe(const imagen& A, const pixel& p, const pixel
             res = true;
         } else {
             blacklist.push_back(p);
-            if(pixelValidoEncendido(A, {p[0]+1, p[1]}) && notIn(blacklist, {p[0]+1, p[1]})){
+            if(pixelValidoEncendido(A, {p[0]+1, p[1]}) && !pertenece(blacklist, {p[0]+1, p[1]})){
                 blacklist.push_back({p[0]+1, p[1]});
                 res |= sonPixelesConectadosViniendoDe(A, {p[0]+1, p[1]}, q, 4, blacklist);
             }
 
-            if(pixelValidoEncendido(A, {p[0]-1, p[1]}) && notIn(blacklist, {p[0]-1, p[1]})){
+            if(pixelValidoEncendido(A, {p[0]-1, p[1]}) && !pertenece(blacklist, {p[0]-1, p[1]})){
                 blacklist.push_back({p[0]-1, p[1]});
                 res |= sonPixelesConectadosViniendoDe(A, {p[0]-1, p[1]}, q, 4, blacklist);
             }
-            if(pixelValidoEncendido(A, {p[0], p[1]+1}) && notIn(blacklist, {p[0], p[1]+1})){
+            if(pixelValidoEncendido(A, {p[0], p[1]+1}) && !pertenece(blacklist, {p[0], p[1]+1})){
                 blacklist.push_back({p[0], p[1]+1});
                 res |= sonPixelesConectadosViniendoDe(A, {p[0], p[1]+1}, q, 4, blacklist);
             }
-            if(pixelValidoEncendido(A, {p[0], p[1]-1}) && notIn(blacklist, {p[0], p[1]-1})){
+            if(pixelValidoEncendido(A, {p[0], p[1]-1}) && !pertenece(blacklist, {p[0], p[1]-1})){
                 blacklist.push_back({p[0], p[1]-1});
                 res |= sonPixelesConectadosViniendoDe(A, {p[0], p[1]-1}, q, 4, blacklist);
             }
@@ -71,12 +71,96 @@ bool pixelValidoEncendido(const imagen &A, const pixel& p){
     return (0 <= p[0] && 0 <= p[1] && A.size() > p[0] && A[0].size() > p[1] && A[p[0]][p[1]] == 1);
 }
 
-bool notIn(const vector<pixel>& list, const pixel& p){
+bool pertenece(const vector<pixel>& list, const pixel& p){
     int i = 0;
     while (i < list.size() && list[i] != p){
         i++;
     }
-    return i == list.size();
+    return i < list.size();
 }
 
+bool esBorde(const imagen &A, const pixel& p){
+    return (p[0] == 0 || p[0] == A.size()-1 || p[1] == 0 || p[1] == A[0].size()-1);
+}
+bool tocaBackgroundAdK(const imagen &A, const pixel& p, int k){
+    bool res = false;
+    if(A[p[0] + 1][p[1]] == 0 || A[p[0] - 1][p[1]] == 0 || A[p[0]][p[1] + 1] == 0 || A[p[0]][p[1] - 1] == 0){
+        res = true;
+    }
+    if(k == 8){
+        if(A[p[0] + 1][p[1] + 1] == 0 || A[p[0] - 1][p[1] + 1] == 0 || A[p[0] + 1][p[1] - 1] == 0 || A[p[0] - 1][p[1] - 1] == 0){
+            res = true;
+        }
+    }
+    return res;
+}
 
+sqPixel convertirASecuencia(const imagen &A){
+    sqPixel sqA;
+
+    for (int i = 0; i < A.size(); ++i) {
+        for (int j = 0; j < A[0].size(); ++j) {
+            if(A[i][j]==1){
+                sqA.push_back({i, j});
+            }
+        }
+    }
+    return sqA;
+}
+
+sqPixel dilatacion(const sqPixel &A, const sqPixel &B, int n, int m){
+    sqPixel res;
+    sqPixel Bdesplazado;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            Bdesplazado = desplazar(B, {i, j});
+            if(hayInterseccion(A, Bdesplazado)){
+                res.push_back({i, j});
+            }
+        }
+    }
+    return res;
+}
+
+sqPixel erosion(const sqPixel &A, const sqPixel &B, int n, int m){
+    sqPixel res;
+    sqPixel Bdesplazado;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            if(pertenece(A, {i, j})) {
+
+                Bdesplazado = desplazar(B, {i, j});
+                if (contenido(A, Bdesplazado, n, m)) {
+                    res.push_back({i, j});
+                }
+            }
+        }
+    }
+    return res;
+}
+
+sqPixel desplazar(const sqPixel &B, const pixel &p){
+    sqPixel res;
+    for (int i = 0; i < B.size(); ++i) {
+        res.push_back({B[i][0] + p[0], B[i][1] + p[1]});
+    }
+    return res;
+}
+
+bool hayInterseccion(const sqPixel &A, const sqPixel &B){
+    bool res = false;
+    for (int i = 0; i < A.size(); ++i) {
+        res |= pertenece(B, A[i]);
+    }
+    return res;
+}
+
+bool contenido(const sqPixel &contenedor, const sqPixel &contenido, int n, int m){
+    bool res = true;
+    for (int i = 0; i < contenido.size(); ++i) {
+        if(0 <= contenido[i][0] && 0 <= contenido[i][1] && n > contenido[i][0] && m > contenido[i][1]){
+            res &= pertenece(contenedor, contenido[i]);
+        }
+    }
+    return res;
+}
--- template-alumnos/ejercicios.cpp
+++ template-alumnos/ejercicios.cpp
@@ -25,16 +25,39 @@ float devolverPromedioAreas(const imagen &A, int k){
 // Ejercicio 4
 
 sqPixel calcularContorno(const imagen &A, int k){
-    sqPixel edges = {{}};
-	// TODO --> cuerpo de funcion
+    sqPixel edges;
+    for (int i = 0; i < A.size(); ++i) {
+        for (int j = 0; j < A[0].size(); ++j) {
+            if(A[i][j] == 1){
+                if(esBorde(A, {i, j}) || tocaBackgroundAdK(A, {i, j}, k)){
+                    edges.push_back({i, j});
+                }
+            }
+        }
+    }
     return edges;
 }
 
 // Ejercicio 5
 
 void cerrarForma(imagen &A, const imagen &B){
-	// TODO --> cuerpo de funcion
-	return;
+    sqPixel sqB = convertirASecuencia(B);
+    sqPixel sqA = convertirASecuencia(A);
+    int centroB = (B.size() - 1)/2;
+    //"centro" la secuencia para que el punto medio sea el (0,0)
+    for (int i = 0; i < sqB.size(); ++i) {
+        sqB[i] = {sqB[i][0] - centroB, sqB[i][1] - centroB};
+    }
+
+    sqPixel dil = dilatacion(sqA, sqB, A.size(), A[0].size());
+    sqPixel ero = erosion(dil, sqB, A.size(), A[0].size());
+
+    for (int i = 0; i < A.size(); ++i) {
+        for (int j = 0; j < A[0].size(); ++j) {
+                A[i][j] = pertenece(ero, {i, j}) ? 1 : 0;
+            }
+    }
+
 }
 
 // Ejercicio 6
